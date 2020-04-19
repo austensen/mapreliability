c0ab7e9c1d06368ae99f828c91995c73363e6610
Maxwell Austensen <maxwell.austensen@nyu.edu>
2020-04-19 19:09:19 -0400

    initial commit

---
+++ R/addin.R
@@ -0,0 +1,188 @@
+#' Map reliability calculator
+#'
+#' Takes a dataframe with estimate and MOE columns and launches an RStudio addin 
+#' to explore the reliability of various classifications of the estimate values for mapping.
+#'
+#' @param data Dataframe
+#' @param est Estimate column
+#' @param moe Margin of error column
+#' @param viewer Where to display the gadget: `"dialog"`, `"pane"` or
+#'   `"browser"` (see [shiny::viewer()]).
+#'
+#' @export
+#'
+#' @import shiny
+#' @import rlang
+#' @importFrom magrittr `%>%`
+#'
+#' @examples
+#' \dontrun{
+#'
+#' if (interactive()) {
+#'
+#' bk_hh_income <- tidycensus::get_acs(
+#'   geography = "tract",
+#'   state = "NY",
+#'   county = "Kings",
+#'   year = 2018,
+#'   survey = "acs5",
+#'   variables = "B19013_001", # Median Household Income
+#'   key = Sys.getenv("CENSUS_API")
+#' )
+#'
+#' reliability_calculator(bk_hh_income, estimate, moe)
+#'
+#' }
+#'
+#' }
+reliability_calculator <- function(data, est, moe, viewer = getOption(x = "mapreliability.viewer", default = "pane")) {
+  input_data <- prep_input_data(data, as_label(enquo(est)), as_label(enquo(moe)))
+
+  custom_placeholder_breaks <- input_data %>%
+    reliability_table_equal(est, moe, 4, quiet = TRUE) %>%
+    dplyr::pull(.data[["class_breaks"]]) %>%
+    paste(collapse = ", ")
+
+
+  ui <- miniUI::miniPage(
+
+    miniUI::gadgetTitleBar(
+      "Map Reliability Calculator",
+      left = miniUI::miniTitleBarCancelButton(),
+      right = miniUI::miniTitleBarButton("done", "Done", primary = TRUE)
+    ),
+
+    miniUI::miniContentPanel(
+      fluidRow(
+        column(12,
+               h3("Input data"),
+               p("Click and drag to restrict the observations used in the calculations"),
+               plotly::plotlyOutput("plot")
+        )
+      ),
+      fluidRow(
+        column(6,
+               h3("Equal Interval"),
+               numericInput("num_classes_eq", NULL, 4, min = 2, step = 1, width = "70px"),
+               DT::dataTableOutput("equal_table")
+        ),
+        column(6,
+               h3("Quantile"),
+               numericInput("num_classes_qu", NULL, 4, min = 2, step = 1, width = "70px"),
+               DT::dataTableOutput("quant_table")
+        )
+      ),
+
+      fluidRow(
+        column(6,
+               h3("Custom"),
+               textInput("custom_breaks",
+                         label = "Enter lower breaks separated by commas",
+                         value = custom_placeholder_breaks),
+               DT::dataTableOutput("custom_table")
+        ),
+        column(6,
+               h3("Notes"),
+               p('This tool assumes the margins of error for the input data use a 90% confidence level.'),
+               p('The reliability column shows the likelihood, on average, that any given geography is erroneously classed.'),
+               p('A classification is considered to be reliable if total reliability is less 10% and all individual classes are less than 20%.'),
+               br(),
+               h5('Suggested notation for maps that pass this reliability test:'),
+               p("There is less than a 10% that chance any given geography in this map is misclassified due to sampling error."),
+               p("For each individual category, there is less than a 20% chance that any given geography is misclassified due to sampling error.")
+        )
+      )
+    )
+  )
+
+  server <- function(input, output, session) {
+
+    output$plot <- plotly::renderPlotly({
+      input_data %>%
+        plotly::plot_ly(type = "scatter", mode = "markers", x = ~est, y = ~moe) %>%
+        plotly::layout(
+          dragmode = "select",
+          xaxis = list(title = "Estimate"),
+          yaxis = list(title = "Margin of Error (90%)")
+        ) %>%
+        plotly::event_register("plotly_selecting")
+    })
+
+    output$equal_table <- DT::renderDataTable({
+      make_table(input_data, reliability_table_equal, n_classes = input$num_classes_eq)
+    })
+
+    output$quant_table <- DT::renderDataTable({
+      make_table(input_data, reliability_table_quant, n_classes = input$num_classes_qu)
+    })
+
+    output$custom_table <- DT::renderDataTable({
+      breaks <- stringr::str_split(input$custom_breaks, ",")[[1]] %>% readr::parse_number() %>% na.omit()
+      make_table(input_data, reliability_table_custom, class_breaks_low = breaks)
+    })
+  }
+
+  make_table <- function(default_data, breaks_fun, ...) {
+    selected_data <- plotly::event_data("plotly_selected")
+
+    if (is.null(selected_data)) {
+      selected_data <- default_data
+    } else {
+      selected_data <- dplyr::select(selected_data, est = .data[["x"]], moe = .data[["y"]])
+    }
+
+    formatted_data <- selected_data %>%
+      breaks_fun(est, moe, ..., quiet = TRUE) %>%
+      tibble::add_row(
+        class_breaks = "TOTAL",
+        count = .[["tot_count"]][[1]],
+        reliability = .[["tot_reliability"]][[1]]
+      ) %>%
+      dplyr::select(-dplyr::starts_with("tot_"))
+
+
+    DT::datatable(
+      formatted_data,
+      rownames = FALSE,
+      colnames = c("Class Breaks" = "class_breaks", "Count" = "count", "Reliability" = "reliability"),
+      options = list(
+        paging = FALSE, searching = FALSE, ordering = FALSE, info = FALSE,
+        scrollY = "600px", scrollCollapse = TRUE,
+        rowCallback = DT::JS(
+          'function(row, data) {
+          // Color classes for reliability
+          if (data[0] != "TOTAL") {
+            if (parseFloat(data[2]) < 20.0)
+              $("td:eq(2)", row).css("color", "green")
+            else
+              $("td:eq(2)", row).css("color", "red");
+          }
+          // Style total row
+          if (data[0] == "TOTAL") {
+            $("td:eq(0)", row).css("font-weight", "bold").css("text-decoration", "underline")
+            $("td:eq(1)", row).css("font-weight", "bold").css("text-decoration", "underline")
+            if (parseFloat(data[2]) < 10.0)
+              $("td:eq(2)", row).css("color", "green").css("text-decoration", "underline")
+            else
+              $("td:eq(2)", row).css("color", "red").css("text-decoration", "underline");
+          }
+        }'
+        )
+      )
+    ) %>%
+      DT::formatStyle("Reliability", `font-weight` = "bold") %>%
+      DT::formatRound("Reliability", 1)
+  }
+
+
+  if (viewer == "dialog") {
+    viewer <- dialogViewer("Map Reliability Calculator")
+  } else if (viewer == "browser") {
+    viewer <- browserViewer()
+  } else {
+    viewer <- paneViewer(minHeight = 600)
+  }
+
+  runGadget(app = ui, server = server, viewer = viewer)
+
+}
---
+++ R/reliability.R
@@ -0,0 +1,140 @@
+#' Classification reliability tables
+#'
+#' Get a table of reliability results for a classification of a set of
+#' estimates.
+#'
+#' @param data Dataframe
+#' @param est Estimate column
+#' @param moe Margin of error column
+#' @param n_classes Number of classes
+#' @param class_breaks_low A vector of lower break points for a custom classification
+#' @param conf_level Confidence level used for MOEs (default = 90\%)
+#' @param quiet Whether to raise warnings about dropped observations with
+#'   missing data
+#'
+#' @import rlang
+#' @importFrom magrittr `%>%`
+#'
+#' @name reliability_table
+NULL
+
+#' @export
+#' @rdname reliability_table
+reliability_table_equal <- function(data, est, moe, n_classes, conf_level = 0.90, quiet = FALSE) {
+
+  data <- prep_input_data(data, as_label(enquo(est)), as_label(enquo(moe)), quiet = quiet)
+
+  z_score <- stats::qnorm((1.0-conf_level)/2, lower.tail = FALSE)
+
+  data %>%
+    dplyr::mutate(
+      grp_breaks = cut(.data[["est"]], n_classes, dig.lab = 10),
+      grp_lo = stringr::str_replace(.data[["grp_breaks"]], "\\((.+),.*", "\\1") %>% as.numeric(),
+      grp_hi = stringr::str_replace(.data[["grp_breaks"]], "[^,]*,([^]]*)\\]", "\\1") %>% as.numeric(),
+      grp_num = as.integer(.data[["grp_breaks"]])
+    ) %>%
+    calc_reliability(n_classes, z_score)
+}
+
+#' @export
+#' @rdname reliability_table
+reliability_table_quant <- function(data, est, moe, n_classes, conf_level = 0.90, quiet = FALSE) {
+
+  data <- prep_input_data(data, as_label(enquo(est)), as_label(enquo(moe)), quiet = quiet)
+
+  z_score <- stats::qnorm((1.0-conf_level)/2, lower.tail = FALSE)
+
+  grp_qu_lo_vals <- stats::quantile(data[["est"]], ((100/n_classes)*((1:n_classes)-1))/100)
+  grp_qu_lo_vals[1] <- -Inf
+  grp_qu_hi_vals <- stats::quantile(data[["est"]], ((100/n_classes)*((1:n_classes)))/100)
+  grp_qu_hi_vals[n_classes] <- Inf
+
+  data %>%
+    dplyr::mutate(
+      grp_num = purrr::map_int(.data[["est"]], ~which(. >= grp_qu_lo_vals & . < grp_qu_hi_vals)),
+      grp_lo = stats::quantile(.data[["est"]], ((100/n_classes)*(.data[["grp_num"]]-1))/100),
+      grp_hi = stats::quantile(.data[["est"]], ((100/n_classes)*(.data[["grp_num"]]))/100)
+    ) %>%
+    calc_reliability(n_classes, z_score)
+}
+
+#' @export
+#' @rdname reliability_table
+reliability_table_custom <- function(data, est, moe, class_breaks_low, conf_level = 0.90, quiet = FALSE) {
+
+  data <- prep_input_data(data, as_label(enquo(est)), as_label(enquo(moe)), quiet = quiet)
+
+  z_score <- stats::qnorm((1.0-conf_level)/2, lower.tail = FALSE)
+
+  n_classes <- length(class_breaks_low)
+
+  class_breaks_high <- c(class_breaks_low[2:n_classes], max(data[["est"]]))
+
+  grp_cus_lo_vals <- class_breaks_low
+  grp_cus_lo_vals[1] <- -Inf
+  grp_cus_hi_vals <- dplyr::lead(class_breaks_low)
+  grp_cus_hi_vals[n_classes] <- Inf
+
+  data %>%
+    dplyr::mutate(
+      grp_num = purrr::map_int(.data[["est"]], ~which(. >= grp_cus_lo_vals & . < grp_cus_hi_vals)),
+      grp_lo = class_breaks_low[.data[["grp_num"]]],
+      grp_hi = class_breaks_high[.data[["grp_num"]]]
+    ) %>%
+    calc_reliability(n_classes, z_score)
+}
+
+
+# takes a dataframe, and the names of the estimate and moe columns as string,
+# and renames the columns to a standard "est" and "moe", then removes rows with
+# missing values for the estimate or moe and optionally raises a warning about
+# it the number of dropped observations.
+prep_input_data <- function(data, est, moe, quiet = FALSE) {
+  data <- data %>%
+    # remove "sf" from class for spatial dataframe
+    tibble::as_tibble() %>%
+    # rename columns to standard "est" and "moe"
+    dplyr::select(est = .data[[est]], moe = .data[[moe]])
+
+  data_no_missing <- data %>% dplyr::filter(!is.na(.data[["est"]]), !is.na(.data[["moe"]]))
+
+  dropped_rows <- nrow(data) - nrow(data_no_missing)
+  if (!quiet && dropped_rows != 0) {
+    rlang::warn(stringr::str_glue(
+      "There are {dropped_rows} observations with missing estimates or MOE that have been removed."
+    ))
+  }
+
+  data_no_missing
+}
+
+# dataframe with cols: est, moe, grp_num, grp_lo, grp_hi
+calc_reliability <- function(data, n_classes, z_score) {
+  data %>%
+    dplyr::mutate(
+      grp_prob = dplyr::case_when(
+        grp_num == n_classes ~ 100-(stats::pnorm((.data[["est"]] - .data[["grp_lo"]])/(.data[["moe"]]/z_score))*100),
+        grp_num == 1         ~ 100-(stats::pnorm((.data[["grp_hi"]] - .data[["est"]])/(.data[["moe"]]/z_score))*100),
+        TRUE                 ~ (100-(stats::pnorm((.data[["grp_hi"]] - .data[["est"]])/(.data[["moe"]]/z_score))*100)
+                               + 100-(stats::pnorm((.data[["est"]] - .data[["grp_lo"]])/(.data[["moe"]]/z_score))*100))
+      )
+    ) %>%
+    dplyr::group_by(.data[["grp_lo"]]) %>%
+    dplyr::summarise(
+      grp_count = n(),
+      grp_cum_prob = sum(.data[["grp_prob"]])
+    ) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+      grp_avg_prob = .data[["grp_cum_prob"]] / .data[["grp_count"]],
+      grp_tot_count = sum(.data[["grp_count"]]),
+      grp_tot_prob = sum(.data[["grp_cum_prob"]]) / .data[["grp_tot_count"]]
+    ) %>%
+    dplyr::select(
+      class_breaks = .data[["grp_lo"]],
+      count = .data[["grp_count"]],
+      tot_count = .data[["grp_tot_count"]],
+      reliability = .data[["grp_avg_prob"]],
+      tot_reliability = .data[["grp_tot_prob"]]
+    )
+}
